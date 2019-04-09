e3c532a7a70a06808189fc011a0e5a7b484dddbb
NineNineFive <38846235+NineNineFive@users.noreply.github.com>
2019-04-09 23:39:28 +0200

    git speechrec

--- thirdPrototype/js/ourfiles/speechReg.js
+++ thirdPrototype/js/ourfiles/speechReg.js
@@ -1,70 +1,69 @@
 
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+var recognition = new window.SpeechRecognition();
+
+
+var finalTranscript = '';
+var interimTranscript = '';
+var newFinal = false;
+
 class SpeechReg {
-    constructor () {
-        var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-        var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-        var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+    constructor() {
+        this.setupSpeecRecog();
+    }
 
-        //var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
-        var colors = ['fire', "shoot", "close", "left", "right", "pew pew","A2", "A5"];
 
-        var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+    setupSpeecRecog() {
+        var instance = this;
 
-        var recognition = new SpeechRecognition();
-        var speechRecognitionList = new SpeechGrammarList();
-        speechRecognitionList.addFromString(grammar, 1);
-        recognition.grammars = speechRecognitionList;
-        recognition.continuous = true;
-        recognition.lang = 'en-US';
-        recognition.interimResults = true;
-        recognition.maxAlternatives = 10;
-
-        var diagnostic = document.querySelector('.output');
-
-        recognition.start();
-        recognition.onspeechstart = function() {
-
-            console.log('Ready to receive a color command.');
+        this.words = ['fire', "shoot", "close", "left", "right", "pew pew", "A2", "A5"];
+        this.grammar = '#JSGF V1.0; grammar this.words; public <word> = ' + this.words.join(' | ') + ' ;'
+        this.recognition = new SpeechRecognition();
+        this.speechRecognitionList = new SpeechGrammarList();
+        this.speechRecognitionList.addFromString(this.grammar, 1);
+        this.recognition.grammars = this.speechRecognitionList;
+        this.recognition.lang = 'en-US';
+        this.recognition.interimResults = true;
+        this.recognition.maxAlternatives = 10;
+        this.recognition.continuous = false;
+        this.recognition.onspeechend = function () {
+            console.log('Speech has stopped being detected... restarting');
+            instance.setupSpeecRecog();
         }
+        this.recognition.onresult = (event) => {
 
-        recognition.onresult = function(event) {
-            var last = event.results.length - 1;
-            var color = event.results[last][0].transcript.split(" ");
-            console.log(event);
-            //var color = event.results[last][0].transcript.split(" ");
+            for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+                let transcript = event.results[i][0].transcript.split(" ");
+                if (event.results[i].isFinal) {
+                    finalTranscript = transcript;
+                    newFinal = true;
+                    console.log(finalTranscript);
+                    this.recognition.stop();
+                    instance.setupSpeecRecog();
 
-            for(var i = 0; i < color.length; i++){
-                console.log("color: "+color[i]);
+                } else {
+                    interimTranscript = transcript;
+                    for(var i = 0; i < transcript.length; i++) {
+                        console.log(transcript[i]);
+                    }
+
+                }
             }
-            console.log('Confidence: ' + event.results[0][0].confidence);
-        }
-
-        recognition.onspeechend = function() {
-            //recognition.stop();
 
         }
-
-        recognition.onnomatch = function(event) {
-            console.log("I didn't recognise that color.");
-        }
-
-        recognition.onerror = function(event) {
-            console.log('Error occurred in recognition: ' + event.error);
-        }
+        this.recognition.start();
     }
 
-    test(){
-
-
-
-    }
 
 
 
 }
 
+var test = new SpeechReg();
 
-var speechreg = new SpeechReg();
+
 
 
 
